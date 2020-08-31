cb87f272feaa6b57b5792ed1c6ecdad71a6e8d40
Raja Karna <66440694+rkarna@users.noreply.github.com>
2020-08-31 17:33:57 -0400

    Assignment00
    Assignment00 Copy

---
+++ completed/assignment00/assignment_00_KarnaRajasekharReddy.R
@@ -0,0 +1,51 @@
+# Assignment: ASSIGNMENT 0
+# Name: Karna, RajasekharReddy
+# Date: 2020-08-31
+
+# Basics
+
+## Add 8 and 5
+8 + 5
+
+## Subtract 6 from 22
+22 - 6
+## Multiply 6 by 7
+7 * 6
+
+## Add 4 to 6 and divide the result by 2
+(6 + 4) / 2
+
+## Compute 5 modulo 2
+2 %% 5
+
+## Assign the value 82 to the variable x
+## Print x
+x <- x(82)
+print(x)
+
+## Assign the value 41 to the variable y
+## Print y
+y <- y(41)
+print(y)
+
+## Assign the output of x + y to the variable z
+## Print z
+z <- x + y
+print(z)
+
+## Assign the string value "DSC520" to the variable class_name
+## Print the value of class_name
+class_name <- print('DSC520')
+
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good <- print(TRUE)
+
+## Check the class of the variable is_good using the `class()` function
+class(is_good)
+
+## Check the class of the variable z using the `class()` function
+class(z)
+
+## Check the class of the variable class_name using the class() function
+class(class_name)
